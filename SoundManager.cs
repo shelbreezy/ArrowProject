0826bdf1dbf0edf13963e2a93a5cb658789567c4
shelbreezy <146044568+shelbreezy@users.noreply.github.com>
2024-01-30 07:43:02 -0800

    Add files via upload

---
+++ SoundManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+
+    [SerializeField] Slider volumeSlider;
+
+
+    // when game starts the volume will start at 1
+    void Start()
+    {
+        if(PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+        }
+
+        else
+        {
+            Load();
+        }
+    }
+
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume"); 
+    }
+
+    // saves player volume settings between game sessions
+
+    private void Save()
+
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+}
+
+
